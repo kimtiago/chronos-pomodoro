59f661549cd8d0674c8ac8ea61bc7283a1dfaffb
Kim Tiago Baptista <kimtiago@gmail.com>
2025-07-08 19:52:41 +0000

    Propriedades (props) e children de componentes no React

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,9 @@ export function App() {
 
   return (
     <>
-      <Heading />
+      <Heading attr={123} attr2='String'>
+        Olá mundo 1
+      </Heading>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum cumque
         incidunt accusamus commodi quasi sit, doloribus in fugiat, repellat
