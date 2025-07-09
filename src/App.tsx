bb8cc4a051ca6ebff1d2ce942784d13ebd03281c
Kim Tiago Baptista <kimtiago@gmail.com>
2025-07-09 19:12:51 +0000

    Criando o componente de CountDown

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
-
 import './styles/theme.css';
 import './styles/global.css';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
 export function App() {
   return (
@@ -14,6 +14,10 @@ export function App() {
 
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
       </Container>
     </>
   );
